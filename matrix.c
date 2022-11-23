56dffc224e6e594f6527f8d519cb56e2e825e9c3
inesvar <90340779+inesvar@users.noreply.github.com>
2022-11-23 09:26:18 +0100

    Ajout des instructions dans matrix

--- matrix.c
+++ matrix.c
@@ -14,3 +14,79 @@ char matrix[12][10] = {
     {CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_RED, CASE_ORANGE, CASE_WHITE, CASE_WHITE, CASE_WHITE},
     {CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE, CASE_WHITE}
 };
+
+
+struct Coords coords = {5, 10};
+enum Direction direction = RIGHT;
+
+void printCoords() {
+    printf("x : %d; y : %d\n", coords.x, coords.y);
+}
+
+void move() {
+    switch (direction) {
+        case UP:
+            coords.y--;
+            break;
+        case DOWN:
+            coords.y++;
+            break;
+        case LEFT:
+            coords.x--;
+            break;
+        case RIGHT:
+            coords.x++;
+            break;
+    }
+}
+
+void turnRight() {
+    direction = (direction + 1) % 4;
+}
+
+void turnLeft() {
+    direction = (direction + 3) % 4;
+}
+
+char isRed() {
+    if (matrix[coords.y][coords.x] == CASE_RED) {
+        return 1;
+    }
+    return 0;
+}
+
+char isOrange() {
+    if (matrix[coords.y][coords.x] == CASE_ORANGE) {
+        return 1;
+    }
+    return 0;
+}
+
+char isBlue() {
+    if (matrix[coords.y][coords.x] == CASE_BLUE) {
+        return 1;
+    }
+    return 0;
+}
+
+void paintRed() {
+    matrix[coords.y][coords.x] = CASE_RED;
+}
+
+void paintBlue() {
+    matrix[coords.y][coords.x] = CASE_BLUE;
+}
+
+char gameLost() {
+    if (matrix[coords.y][coords.x] == CASE_WHITE) {
+        return 1;
+    }
+    return 0;
+}
+
+char gameWon() {
+    if (matrix[coords.y][coords.x] == CASE_STAR) {
+        return 1;
+    }
+    return 0;
+}
--- matrix.h
+++ matrix.h
@@ -9,4 +9,30 @@ enum Color {
 
 extern char matrix[12][10];
 
+enum Direction {
+    RIGHT, DOWN, LEFT, UP
+};
+
+struct Coords {
+    unsigned char x;
+    unsigned char y;
+};
+//careful, the position is at matrix[y][x]
+
+extern struct Coords coords;
+extern enum Direction direction;
+
+void printCoords();
+
+void move();
+void turnRight();
+void turnLeft();
+char isRed();
+char isOrange();
+char isBlue();
+void paintRed();
+void paintBlue();
+char gameLost();
+char gameWon();
+
 #endif
